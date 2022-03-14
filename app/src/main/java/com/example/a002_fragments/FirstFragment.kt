800a8169262aa1e80743143fa71ea68fba3e8336
unknown <richardchandra99@gmail.com>
2022-03-15 00:24:05 +0700

    Initial commit for Android fragments learning hands-on code

---
+++ app/src/main/java/com/example/a002_fragments/FirstFragment.kt
@@ -0,0 +1,15 @@
+package com.example.a002_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+
+}
